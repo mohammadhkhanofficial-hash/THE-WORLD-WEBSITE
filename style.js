cd1a63e25bc928a58181b0d3214c8d82aafcc4e6
mohammadhkhanofficial-hash <mohammadhkhanofficial@gmail.com>
2026-07-07 22:23:02 +0500

    Add files via upload

--- style.js
+++ style.js
@@ -9,30 +9,7 @@ function typeWriter() {
     }
 }
 
-// Automatic Button Generation and Interaction
 window.onload = function() {
-    // Typewriter chalaen
+    // Website khulte hi typewriter animation chal jaye
     typeWriter();
-
-    // Pehle button ko HTML container ke andar create karein
-    const container = document.getElementById("button-container");
-    if (container) {
-        const btn = document.createElement("button");
-        btn.className = "btn";
-        btn.innerText = "Download PDF";
-        
-        // Click Event Listener lagaein
-        btn.addEventListener('click', function() {
-            this.innerText = "Downloading...";
-            setTimeout(() => {
-                alert("Thanks for your cooperation! May you like this book.");
-                this.innerText = "Download PDF";
-                // Agar aapka asli pdf file ka link ho toh niche wali line se download shuru ho sakta hai:
-                 window.location.href = "THE WORLD.pdf"; 
-            }, 1000);
-        });
-
-        // Button ko container mein shamil karein
-        container.appendChild(btn);
-    }
 };
